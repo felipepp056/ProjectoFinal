fa5db3ced3a623c18b03236406c0cc62e7c961b8
Felipe Peña <edgarfpena@hotmail.com>
2023-03-22 21:47:41 -0500

    Se incluye scss, SEO y animaciones

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Script para hacer el botón del modo nocturno funcional
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
